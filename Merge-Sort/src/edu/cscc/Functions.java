fdb872a3bf2893349110936000fb1c90a86a1a02
CORP\linser1 <bobby.linse@goodifycharities.org>
2019-11-19 16:08:07 -0500

    Merge Sort: complete MergeSort

--- Merge-Sort/src/edu/cscc/Functions.java
+++ Merge-Sort/src/edu/cscc/Functions.java
@@ -4,35 +4,27 @@ import java.util.Arrays;
 
 public class Functions {
 
-    public static int[] mergeSort(int[] a) {
+    public static int[] mergeSort(int[] fullArr) {
+        int size = fullArr.length;
+        int[] leftArr = new int[size/2];
+        int[] rightArr = new int[size - size/2];
 
-        int size = a.length;
-
-        //base case
-        if(size == 1){
-            return a;
-        }
-        else {
-            int[] leftArr = new int[(size+1)/2];
-            int[] rightArr = new int[size - leftArr.length];
-
-            for (int i = 0; i < size; i++)
-            {
-                if (i < leftArr.length)
-                    leftArr[i] = a[i];
-                else
-                    rightArr[i - leftArr.length] = a[i];
-            }
-
-            System.out.println("Complete: "+Arrays.toString(a));
-            System.out.println("Left: "+Arrays.toString(leftArr));
-            System.out.println("Right: "+Arrays.toString(rightArr));
-
-            return a;
-
+        //Base Case
+        if (size <= 1){
+            return fullArr;
         }
 
+        for (int i = 0; i < leftArr.length; i++){
+            leftArr[i] = fullArr[i];
+        }
+
+        for (int i = 0; i < rightArr.length; i++){
+            rightArr[i] = fullArr[i + size/2];
+        }
+
+        return mergeArrays(mergeSort(leftArr), mergeSort(rightArr));
     }
+
 
     public static int rBinarySearch(int[] data, int key){
 
@@ -42,6 +34,32 @@ public class Functions {
     public static int iBinarySearch(int[] data, int key){
 
         return -1;
+    }
+
+    public static int[] mergeArrays(int[] leftArr, int[] rightArr) {
+        int[] fullArr = new int[leftArr.length + rightArr.length];
+
+        int i = 0, j = 0;
+
+        for (int k = 0; k < fullArr.length; k++) {
+            if(i >= leftArr.length) {
+                fullArr[k] = rightArr[j++];
+            }
+
+            else if(j >= rightArr.length) {
+                fullArr[k] = leftArr[i++];
+            }
+
+            else if(leftArr[i] <= rightArr[j]) {
+                fullArr[k] = leftArr[i++];
+            }
+
+            else {
+                fullArr[k] = rightArr[j++];
+            }
+        }
+
+        return fullArr;
     }
 
 }
--- Merge-Sort/src/edu/cscc/Main.java
+++ Merge-Sort/src/edu/cscc/Main.java
@@ -1,11 +1,14 @@
 package edu.cscc;
 
 
+import java.util.Arrays;
+
 public class Main {
   public static void main(String[] args){
             int[] a = {1,3,5,7,9,2,4,6,8};
+            System.out.println(Arrays.toString(a));
             a = Functions.mergeSort(a);
-
+            System.out.println(Arrays.toString(a));
 
             //int idx = Functions.iBinarySearch(a,4);
         }
