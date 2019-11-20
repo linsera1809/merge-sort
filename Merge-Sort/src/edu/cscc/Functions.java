d9c7021d1bf50aaa5cee4db83e2635cf7e76e134
CORP\linser1 <bobby.linse@goodifycharities.org>
2019-11-19 20:19:19 -0500

    gd that took forever.
    lab done.

--- Merge-Sort/src/edu/cscc/Functions.java
+++ Merge-Sort/src/edu/cscc/Functions.java
@@ -2,8 +2,20 @@ package edu.cscc;
 
 import java.util.Arrays;
 
+/**
+ * Perform a series of functions to search and sort the data
+ * @author Bobby Linse
+ * Date: November 2019
+ */
 public class Functions {
 
+    /**
+     * Given an array of int values, the mergeSort
+     * function will split the arrays into two,
+     * sort the two arrays, and combine them together.
+     * @param fullArr
+     * @return
+     */
     public static int[] mergeSort(int[] fullArr) {
         int size = fullArr.length;
         int[] leftArr = new int[size/2];
@@ -26,16 +38,88 @@ public class Functions {
     }
 
 
+    /**
+     * Given an int[], recursively search for the key-value
+     * @param data
+     * @param key
+     * @return
+     */
     public static int rBinarySearch(int[] data, int key){
 
+        int leftSide = 0;
+        int rightSide = data.length - 1;
+        if (rightSide >= leftSide) {
+
+            int mid = leftSide + (rightSide - leftSide) / 2;
+
+            //Base Case
+            if (data[mid] == key || data[mid-1] == key) {
+        //        System.out.println("Found key! "+ key);
+                return mid;
+            }
+
+            if (data[mid] > key) {
+        //        System.out.println("Ignoring larger half!");
+        //        System.out.println("Data (Recursive): "+ Arrays.toString(data));
+                data = splitArrays(data, leftSide, mid);
+                return rBinarySearch(data, key);
+            }
+
+        //        System.out.println("Ignoring lower half!");
+        //        System.out.println("Data (Recursive): "+ Arrays.toString(data));
+                data = splitArrays(data, mid + 1, rightSide);
+                return rBinarySearch(data, key);
+
+        }
         return -1;
     }
 
+    /**
+     * Given an array and a key to search for, perform a binary
+     * search to locate the key
+     * @param data
+     * @param key
+     * @return
+     */
     public static int iBinarySearch(int[] data, int key){
 
+        int leftSide= 0;
+        int rightSide = data.length - 1;
+
+        System.out.println("Data: "+Arrays.toString(data));
+
+        while (leftSide <= rightSide) {
+
+            int m = leftSide + (rightSide - leftSide) / 2;
+
+
+            if (data[m] == key) {
+            //    System.out.println("Found key! "+m);
+                return m;
+            }
+
+            if (data[m] < key) {
+            //    System.out.println("Ignoring lower half!");
+                leftSide = m + 1;
+            }
+
+            else {
+            //   System.out.println("Ignoring larger half!");
+                rightSide = m - 1;
+            }
+        }
+
+        System.out.println("Key not found");
         return -1;
     }
 
+    /**
+     * Given two arrays, the mergeArrays function will combine the two
+     * into one.
+     * @param leftArr
+     * @param rightArr
+     * @return
+     */
     public static int[] mergeArrays(int[] leftArr, int[] rightArr) {
         int[] fullArr = new int[leftArr.length + rightArr.length];
 
@@ -60,6 +144,29 @@ public class Functions {
         }
 
         return fullArr;
+    }
+
+    /**
+     * Given an int[] split it into 2. This is to be used in the recursive
+     * binary search
+     * @param data
+     * @param startPoint
+     * @param midPoint
+     * @return
+     */
+    public static int[] splitArrays(int[] data, int startPoint, int midPoint) {
+
+        int[] dataSubarray = new int[midPoint-startPoint+1];
+
+    //    System.out.println("Data: "+ Arrays.toString(data));
+    //    System.out.println("Starting position (startPoint): "+ startPoint + "th pos = " + data[startPoint]);
+    //    System.out.println("Stopping position (midpoint): "+ midPoint + "th pos = " + data[midPoint]);
+
+        System.arraycopy(data, startPoint, dataSubarray, startPoint, dataSubarray.length);
+
+    //    System.out.println("DataSubarray: "+ Arrays.toString(dataSubarray));
+    //    System.out.println("");
+        return dataSubarray;
     }
 
 }
--- Merge-Sort/src/edu/cscc/Main.java
+++ Merge-Sort/src/edu/cscc/Main.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args){
-            int[] a = {1,3,5,7,9,2,4,6,8};
+            int[] a = {1,3,5,7,9,2,4,6,8,11};
             System.out.println(Arrays.toString(a));
             a = Functions.mergeSort(a);
             System.out.println(Arrays.toString(a));
 
-            //int idx = Functions.iBinarySearch(a,4);
+            int idx = Functions.rBinarySearch(a,4);
         }
     }
